bfb3877b4f061938bb1a695123757feb49bdc15a
BrianLeoGoldman <leonel89011@gmail.com>
2018-04-06 00:11:16 -0300

    Modelo de practica 3 funcionando

---
+++ entregas/3.practica_3/main.py
@@ -0,0 +1,31 @@
+from practica3.hardware import *
+from practica3.so import *
+from practica3 import log
+
+
+##
+##  MAIN 
+##
+if __name__ == '__main__':
+    log.setupLogger()
+    log.logger.info('Starting emulator')
+
+    ## setup our hardware and set memory size to 25 "cells"
+    HARDWARE.setup(25)
+    
+    ## new create the Operative System Kernel
+    kernel = Kernel()
+
+    # Ahora vamos a intentar ejecutar 3 programas a la vez
+    ###################
+    prg1 = Program("prg1.exe", [ASM.CPU(2), ASM.IO(), ASM.CPU(3), ASM.IO(), ASM.CPU(2)])
+    prg2 = Program("prg2.exe", [ASM.CPU(4), ASM.IO(), ASM.CPU(1)])
+    prg3 = Program("prg3.exe", [ASM.CPU(3)])
+
+    # execute all programs "concurrently"
+    kernel.execute(prg1)
+    kernel.execute(prg2)
+    kernel.execute(prg3)
+
+    ## start
+    HARDWARE.switchOn()
